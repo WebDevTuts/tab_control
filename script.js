0cd693a62eca649dae6de48a9e55e6145df277e8
Edwin Chen <edwinchen85@gmail.com>
2017-05-19 21:35:33 +0800

    Define variables

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ document.addEventListener('click', tabClick);
 
 // Define a function that filters the unwanted click events on the document
 function tabClick(event) {
-
+  var elem = event.target,
+      elemHREF = elem.getAttribute('href'),
+      tabs = document.querySelectorAll('.tabs li a'),
+      tabContents = document.querySelectorAll('.tab-contents li');
 }
