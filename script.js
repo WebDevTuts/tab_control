211a78c794f459b1d5a39f9f8c43993e45cfbaea
Edwin Chen <edwinchen85@gmail.com>
2017-05-19 21:34:12 +0800

    Define a function that filters the unwanted click events on the document

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Add event listener for document click
 document.addEventListener('click', tabClick);
+
+// Define a function that filters the unwanted click events on the document
+function tabClick(event) {
+
+}
